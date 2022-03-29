22d8c0d60261fe1b879da2643abf26a3cfb33547
Loigi Deliguer <loigideliguer1@gmail.com>
2022-03-29 18:46:32 +0800

    update app

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { firebase, db } from "./firebase";
+
+import AuthNavigator from "./app/navigations/AuthNavigator";
+import colors from "./app/config/colors";
 
 export default function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const userHandler = (user) =>
+    user ? setCurrentUser(user) : setCurrentUser(null);
+
+  const unSubscribe = async () => {
+    return await firebase
+      .auth()
+      .onAuthStateChanged((user) => userHandler(user));
+  };
+
+  useEffect(() => {
+    unSubscribe();
+
+    // db.collectionGroup("listings").onSnapshot((snapshot) => {
+    //   setListings(snapshot.docs.map((doc) => doc.data()));
+    // });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={navigationTheme}>
+      {/* {currentUser ? <AppNavigator /> : <AuthNavigator />} */}
+      <AuthNavigator />
+      <StatusBar backgroundColor={colors.primary} />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
